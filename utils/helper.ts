009b59f1d7e917f5ded8808ac37c363017681043
Kenta Shimosawa <shimon101018@gmail.com>
2022-09-20 19:46:04 +0900

    refactor: 💡 function name

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,6 +1,6 @@
 import { LangType } from "../slicers/translation";
 
 // LangTypeからなんの言語かを日本語で取得
-export const toJapaneseFromLangType = (lang: string) => {
-    return lang === LangType.JAPANESE ? "日本語" : "中国語"
+export const toJapaneseFromLangType = (langType: string) => {
+    return langType === LangType.JAPANESE ? "日本語" : "中国語"
 }
